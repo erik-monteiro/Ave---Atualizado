a129a8504d9389062a78879f01c8d37f48db3d51
Wesley PDS9999 <105060496+WesleyPDS9999@users.noreply.github.com>
2022-06-26 19:48:15 +0000

    Alteração no nome do arquivo

--- Ave.java
+++ Ave.java
@@ -26,44 +26,58 @@ public class Ave
         this.linha = linha;
         this.coluna = coluna;
     }
-
+    /**
+	  * Obtem o nome do passaro em portugues.
+	  */
     public String getNomePortugues()
     {
         return nomePortugues;
     }
-	/**
-	* Obtem o nome em latim
-	*/
+	  /**
+	  * Obtem o nome do passaro em latim.
+	  */
     public String getNomeLatim()
     {
         return nomeLatim;
     }
-
+    /**
+	  * Obtem o nome do passaro em ingles.
+	  */
     public String getNomeIngles()
     {
         return nomeIngles;
     }
-
+    /**
+	  * Obtem a cor do passaro.
+	  */
     public String getCor()
     {
         return cor;
     }
-
+    /**
+	  * Obtem o tamanho do passaro.
+	  */
     public String getTamanho()
     {
         return tamanho;
     }
-
+    /**
+	  * Obtem o habitat onde o passaro pode ser encontrado.
+	  */
     public String getHabitat()
     {
         return habitat;
     }
-
+    /**
+	  * Obtem a familia da qual o do passaro pertence.
+	  */
     public String getFamilia()
     {
         return familia;
     }
-
+    /**
+	  * Obtem a pagina da qual o passaro se encontra no guia de aves.
+	  */
     public String getPagina()
     {
         return pagina;
